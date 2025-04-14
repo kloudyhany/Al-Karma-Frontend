e8532627d9533793d3288cd5ea726fbd7462e7dd
melad <49677183+miladw@users.noreply.github.com>
2025-04-14 17:30:56 +0200

    headeeeeeeer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, FooterComponent],
+  imports: [RouterOutlet, ReactiveFormsModule, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
