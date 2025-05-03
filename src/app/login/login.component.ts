24a5061263ca9b58dc7fc6d68d142941be3b2d26
kloudy <kloudyhany@gmail.com>
2025-05-03 04:33:40 +0200

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -24,7 +24,7 @@ export class loginComponent {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required,  Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
       password: ['', Validators.required],
     });
   }
