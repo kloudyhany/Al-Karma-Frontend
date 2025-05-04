c4b7f84e4d0ca51035daa4a825c73b211df0da29
kloudy <kloudyhany@gmail.com>
2025-05-04 22:19:07 +0200

    updateee

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component  , Inject, PLATFORM_ID} from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive , CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 }) 
 export class HeaderComponent {
+  // isLoggedIn = false;
+
+  // constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  // ngOnInit(): void {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     const userCookie = this.getCookie('user');
+  //     this.isLoggedIn = userCookie !== null && userCookie.trim() !== '';
+  //   }
+  // }
+
+  // getCookie(name: string): string | null {
+  //   const value = `; ${document.cookie}`;
+  //   const parts = value.split(`; ${name}=`);
+  //   if (parts.length === 2) {
+  //     const cookieValue = parts.pop()!.split(';').shift();
+  //     return cookieValue ? decodeURIComponent(cookieValue) : null;
+  //   }
+  //   return null;
+  // }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -47,7 +47,7 @@ export class loginComponent {
           Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'),
         ],
       ],
-      password: ['', Validators.required],
+      password: ['', [Validators.required , Validators.pattern(/^(?=(.*[a-z]))(?=(.*[A-Z]))(?=(.*\d))(?=(.*[@$!%*?&]))[A-Za-z\d@$!%*?&]{8,20}$/)]]
     });
   }
 
