d8660ef8c09e8bb3b551ec20c4978c6ff465b1b6
melad <49677183+miladw@users.noreply.github.com>
2025-05-07 21:07:42 +0300

    ay haga

--- src/app/core/auth.interceptor.ts
+++ src/app/core/auth.interceptor.ts
@@ -1,15 +1,33 @@
+import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
+  const router = inject(Router); 
+
+  let authReq = req;
 
   if (token) {
-    req = req.clone({
+    authReq = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
+        Authorization: `Bearer ${token}`
+      }
     });
   }
 
-  return next(req);
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        localStorage.removeItem('token');
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  );
 };
+
+
